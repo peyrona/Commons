6892b79ee1c8e03faac9169c121e44f5cfc1495c
peyrona <peyrona@gmail.com>
2017-05-10 01:19:55 +0200

    Update UtilLang.java
    Bug solved: 2 useless methods removed and 2 added.

--- src/com/peyrona/commons/util/UtilLang.java
+++ src/com/peyrona/commons/util/UtilLang.java
@@ -33,55 +33,6 @@ public class UtilLang
     private UtilLang() {}  // Avoid creation of instances of this class
     //------------------------------------------------------------------------//
 
-    /**
-     * This method performs follwing call:<br>
-     * <code>invoke( obj, methodName, returnWhenNull, new Object[] {} )</code>
-     *
-     * @param obj The one to invoke the method to.
-     * @param methodName To invoke on the object.
-     * @param returnWhenNull Value to return if passed object was null.
-     * @return Return of called method
-     */
-    public static Object invoke( Object obj, String methodName, Object returnWhenNull )
-    {
-        return invoke( obj, methodName, returnWhenNull, new Object[] {} );
-    }
-
-    /**
-     * Invokes a method for an object, but if the object were null, last parameter is returned.
-     * <p>
-     * Note: if an exception occurs during method invocation (i.e. NoSuchMethodException), then
-     * the exception instance is returned.
-     *
-     * @param obj The one to send the methos to.
-     * @param methodName To invoke over the object.
-     * @param returnWhenNull Value to return if passed object was null.
-     * @param args Arguments to pass to called method.
-     * @return The result to invoke the method over the object, or returnWhenNull if object was
-     *         null, or the exception if any.
-     */
-    public static Object invoke( Object obj, String methodName, Object returnWhenNull, Object... args )
-    {
-        Object ret = returnWhenNull;
-
-        if( obj != null )
-        {
-            Class params[] = {};
-
-            try
-            {
-                Method method = obj.getClass().getMethod( methodName, params );
-                ret = method.invoke( obj, args );
-            }
-            catch( NoSuchMethodException | IllegalAccessException | InvocationTargetException | SecurityException ex )
-            {
-                ret = ex;
-            }
-        }
-
-        return ret;
-    }
-
     public static Field getField( Class<?> clazz, String name )
     {
         for( Field f : clazz.getDeclaredFields() )
@@ -104,6 +55,34 @@ public class UtilLang
             if( f != null )
             {
                 return f;
+            }
+        }
+
+        return null;
+    }
+	
+    public static Method getMethod( Class<?> clazz, String name )
+    {
+        for( Method m : clazz.getDeclaredMethods() )
+        {
+            if( m.getName().equalsIgnoreCase( name ) )
+            {
+                return m;
+            }
+        }
+
+        return null;
+    }
+
+    public static Method getMethodInHierarchy( Class<?> clazz, String name )
+    {
+        for( Class<?> c = clazz; c != null; c = c.getSuperclass() )
+        {
+            Method m = getMethod( c, name );
+
+            if( m != null )
+            {
+                return m;
             }
         }
 
